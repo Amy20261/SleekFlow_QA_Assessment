ef1a8f19f2a594a6a53e80771a48befea03e145e
Amy20261 <131218772+Amy20261@users.noreply.github.com>
2023-11-27 18:31:50 +0800

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,17 @@
+﻿from time import sleep
+from selenium import webdriver
+
+browser=webdriver.Firefox()
+browser.get('https://www.google.com/search?q=iphone13&tbm=shop')
+browser.maximize_window()
+sleep(3)
+
+names=browser.find_elements_by_xpath('//h3')
+
+nameList=[]
+for name in range(len(names)):
+    nameList.append(names[name].text)    
+print(*nameList, sep = "\n")
+
+
+
